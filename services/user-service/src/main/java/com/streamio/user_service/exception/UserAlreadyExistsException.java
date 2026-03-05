3ca6f3777b8137c5171b35fa1b0c08bc62b55725
anouar eil barry <elbarryanwar37@gmail.com>
2026-03-05 12:31:20 +0000

    add custom exceptions for user service: UserAlreadyExistsException and UserNotFoundException

---
+++ services/user-service/src/main/java/com/streamio/user_service/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.streamio.user_service.exception;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
